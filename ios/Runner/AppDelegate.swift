67513ea4ec8a438daa71de80fbb6137e6a6074cb
beshoyfayez63 <beshoyfayez751@yahoo.com>
2022-07-04 23:41:14 +0200

    remove google maps api

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyD0Ap0RYmfw_MjjQjBR3m8bShKePD2LYYQ")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
